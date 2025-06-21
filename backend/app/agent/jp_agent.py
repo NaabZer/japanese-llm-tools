52861c1eeecc32869b88b0d8f329e510879fad06
NaabZer <anton.gefvert@gmail.com>
2025-06-21 13:40:58 +0200

    Make word sentence generation into a function

--- backend/app/agent/jp_agent.py
+++ backend/app/agent/jp_agent.py
@@ -9,11 +9,11 @@ from pydantic import BaseModel, Field
 
 class JapaneseReturnModel(BaseModel):
     target_word: Annotated[str, Field(
-        description="Word used in input to generate sentence")]
-    translated_word: Annotated[str, Field(
-        description="target_word translated to English.")]
+        description="The japanese word used in input to generate sentence")]
+    # translated_word: Annotated[str, Field(
+    #     description="target_word translated to English.")]
     target_sentence: Annotated[str, Field(
-        description=('Sentence generated that includes target_word, '
+        description=('Japanese sentence generated that includes target_word, '
                      'where the target_word is bold.'))]
     translated_sentence: Annotated[str, Field(
         description=('target_sentence translated to English, '
@@ -36,15 +36,21 @@ agent = Agent(
         system_prompt=system_prompt
         )
 
-if __name__ == '__main__':
-    in_word = sys.argv[1]
+
+def generate_word_sentence(word: str) -> JapaneseReturnModel:
     prompt = ('You will be given a word in japanese, for this word, '
               'generate an example sentence in japanese containing the word. '
               'The other words in the sentence should be at maximum N3 level, '
               'to make sure that all other words in the sentence are known. '
-              'Also give translations of both the sentence and word. '
-              f"The word is {in_word}"
+              'Also give a translation of the constructed sentence. '
+              f"The word is {word}"
               )
     result = agent.run_sync(prompt)
+    return result
+
+
+if __name__ == '__main__':
+    in_word = sys.argv[1]
+    result = generate_word_sentence(in_word)
     print(result.output.model_dump_json(indent=2))
     print(type(result.output))
