ee04b6c2e57f362c79b5ef9c31d5d1beeb9cbcc6
NaabZer <anton.gefvert@gmail.com>
2025-06-23 12:22:49 +0200

    route name change

--- backend/app/api/routers/sentences.py
+++ backend/app/api/routers/sentences.py
@@ -5,6 +5,6 @@ from app.agent.jp_agent import generate_word_sentence, JapaneseReturnModel
 router = APIRouter(prefix='/sentences', tags=['sentences'])
 
 
-@router.get('/jp_sent')
+@router.get('/get_example')
 def get_example(target_word: str) -> JapaneseReturnModel:
     return generate_word_sentence(target_word).output
