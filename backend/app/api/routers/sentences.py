933964e9966a212b9ac2b82dcc4a8043a9c5741d
NaabZer <anton.gefvert@gmail.com>
2025-06-24 13:09:28 +0200

    Add language enum to back-end api

--- backend/app/api/routers/sentences.py
+++ backend/app/api/routers/sentences.py
@@ -1,10 +1,19 @@
+from enum import Enum
 from fastapi import APIRouter
 from app.agent.jp_agent import generate_word_sentence, JapaneseReturnModel
+
+
+class Language(str, Enum):
+    Japanese = 'japanese'
+    Swedish = 'swedish'
 
 
 router = APIRouter(prefix='/sentences', tags=['sentences'])
 
 
 @router.get('/get_example')
-def get_example(target_word: str) -> JapaneseReturnModel:
+def get_example(
+        target_word: str,
+        target_language: Language
+        ) -> JapaneseReturnModel:
     return generate_word_sentence(target_word).output
