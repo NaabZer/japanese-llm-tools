6237c546be38b6cb954c163bf898dac49769e905
NaabZer <anton.gefvert@gmail.com>
2025-07-04 11:31:02 +0200

    Make modal use route search (almost working fully..)

--- frontend/src/routes/_input_type/route.tsx
+++ frontend/src/routes/_input_type/route.tsx
@@ -1,50 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { AnimatePresence } from 'motion/react';
 import { useMediaQuery } from 'usehooks-ts'
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, useNavigate, useRouter } from '@tanstack/react-router'
+import { fallback, zodValidator } from '@tanstack/zod-adapter'
+import { z } from 'zod'
 
 import { useExampleSentenceSearch } from 'hooks/useExampleSentenceSearch';
 import LanguageSelector from 'components/LanguageSelector';
 import AppTitle from 'components/AppTitle';
 
+const inputSearchSchema = z.object({
+  ShowLangSelect: fallback(z.boolean(), false).default(false)
+})
+
 export const Route = createFileRoute('/_input_type')({
   component: InputTypeComponent,
+  validateSearch: zodValidator(inputSearchSchema)
 })
 
 
 function InputTypeComponent() {
-  const [showLanguageSelector, setShowLanguageSelector] = useState(false);
   const [isLanguageSelectorShowing, setIsLanguageSelectorShowing] = useState(false);
 
   const { clearResults } = useExampleSentenceSearch();
 
+  // TODO: Make into a hook or something
   const [clickAbsoluteOrigin, setClickAbsoluteOrigin] = useState<{ x: number; y: number, w: number, h:number } | null>(null);
 
-  const handleLanguageToggleClick = (compX: number, compY: number, compW: number, compH: number) => {
-    setClickAbsoluteOrigin({ x: compX, y: compY, w: compW, h: compH }); // Save absolute click position
-    setShowLanguageSelector(true); // Open the modal
-    setIsLanguageSelectorShowing(true);
-  };
-
-  const handleCloseLanguageSelector = () => {
-    setShowLanguageSelector(false);
-    setClickAbsoluteOrigin(null); // Clear origin on close
-  };
-
   const isDesktop = useMediaQuery('(min-width: 576px)')
+
+  const { ShowLangSelect } = Route.useSearch();
+  const navigate = useNavigate({from: Route.fullPath});
+  const router = useRouter();
+
+  const handleLanguageToggleClick = useCallback((compX: number, compY: number, compW: number, compH: number) => {
+    setClickAbsoluteOrigin({ x: compX, y: compY, w: compW, h: compH }); // Save absolute click position
+    navigate({
+      search: (prev) => ({...prev, ShowLangSelect: true}),
+    })
+    setIsLanguageSelectorShowing(true);
+  }, [navigate]);
+
+  const handleCloseLanguageSelector = useCallback(() => {
+    router.history.back();
+    //navigate({
+    //  history: 'back'
+    //})
+  }, [router]);
+
+  const handleModalCompletelyClosed = useCallback(() => {
+    setIsLanguageSelectorShowing(false);
+    //setClickAbsoluteOrigin(null); // Clear origin on close
+  }, []);
 
   return (
     <>
       <AppTitle
         onLanguageToggleClick={handleLanguageToggleClick}
-        isLanguageSelectorOpen={isDesktop ? isLanguageSelectorShowing : showLanguageSelector}
+        isLanguageSelectorOpen={isDesktop ? isLanguageSelectorShowing : ShowLangSelect}
       />
       <AnimatePresence>
-        {showLanguageSelector && (
+        {ShowLangSelect && (
           <LanguageSelector 
             onClose={handleCloseLanguageSelector} 
-            onSelect={() => clearResults()}
-            onCompletelyClosed={() => setIsLanguageSelectorShowing(false)}
+            onSelect={clearResults}
+            onCompletelyClosed={handleModalCompletelyClosed}
             absoluteOrigin={clickAbsoluteOrigin} 
           />
         )}
